0f623c39e8bb2c72aedc309dabde8ae690350b63
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-22 02:24:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,21 @@
 def addition(num1, num2)
   sum = num1 + num2
-  sum
 end
 
 def subtraction(num1, num2)
   val = num1 - num2
-  val
 end
 
 def division(num1, num2)
   div = num1 / num2
-  div
 end
 
 def multiplication(num1, num2)
   mult = num1 * num2
-  mult
 end
 
 def modulo(num1, num2)
   mod = num1 % num2
-  mod
 end
 
 def square_root(num)
